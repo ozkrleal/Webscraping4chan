e98e71debd02230a851afd1e4a8b1310c268e1b0
ozkrleal <ozkralm10@hotmail.com>
2019-12-02 04:27:50 +0100

    Update in script Signed-off-by: ozkrleal <ozkralm10@hotmail.com>

--- assignmentsingle.R
+++ assignmentsingle.R
@@ -1,21 +1,17 @@
-library(jsonlite)
 library(rvest)
 library(tm)
 library(topicmodels)
 library(SnowballC)
 library(wordcloud)
 
-
 page_numbers <- 2:10 
 base_url <- "http://boards.4chan.org"
 
 getHundredKeywords <- function(category){
-  
   paging_urls <- paste0(base_url, category, page_numbers)
-  
   head(paging_urls, 3)
-  
   all_links <- NULL
+  
   for (url_base in paging_urls) {
     html_document <- read_html(url_base)
     
@@ -26,10 +22,7 @@ getHundredKeywords <- function(category){
     all_links <- c(all_links, links)
   }
   
-  links
-  
   all_links <- Corpus(VectorSource(all_links))
-  
   all_links <- tm_map(all_links, removeNumbers)
   all_links <- tm_map(all_links, removePunctuation)
   all_links <- tm_map(all_links, stripWhitespace)
@@ -38,15 +31,12 @@ getHundredKeywords <- function(category){
   all_links <- tm_map(all_links, stemDocument)
   
   dtm <- DocumentTermMatrix(all_links)
-  
+
   freq <- colSums(as.matrix(dtm))
-  
-  length(freq)
 
   ord <- order(freq, decreasing=TRUE)
   headwords <- head(freq[ord], 100)
   
- 
   #df <- data.frame('category' = category, 'headwords' = headwords, 'dtm' = dtm, 'freq' = freq, 'ord' =  ord)
   
   return(headwords)
@@ -62,15 +52,12 @@ plotAndSaveWordMap <- function(vector, category){
   dev.off()
 }
 
-#Politically Incorrect section
-polHead <- getHundredKeywords('/pol/')
-plotAndSaveWordMap(polHead, 'pol')
+categories <- c('/pol/', '/fit/', '/biz')
 
-#Fitness section
-fitHead <- getHundredKeywords('/fit/')
-plotAndSaveWordMap(fitHead, 'fit')
-
-#Business and Finance section
-bizHead <- getHundredKeywords('/biz/')
-plotAndSaveWordMap(bizHead, 'biz')
-
+for (category in categories){
+  print(paste0(category, " Section plot..."))
+  headCat <- getHundredKeywords(category)
+  print("Saving as png...")
+  plotAndSaveWordMap(headCat, gsub('/', '', category))
+  print("Done!")
+}
